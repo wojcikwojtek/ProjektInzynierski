58f995d38ae6a66616cc657178781b849d2e312d
wojcikwojtek <wojtekwojcik2310@gmail.com>
2024-07-31 18:14:26 +0200

    Pierwszy commit

---
+++ RatingTouristAttractions/src/main/java/com/inzynierka/RatingTouristAttractions/RatingTouristAttractionsApplication.java
@@ -0,0 +1,13 @@
+package com.inzynierka.RatingTouristAttractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingTouristAttractionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingTouristAttractionsApplication.class, args);
+	}
+
+}
