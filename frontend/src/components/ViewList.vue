c84983770f8998ff587c8b6c6a4743ff104af50c
wojcikwojtek <wojtekwojcik2310@gmail.com>
2024-09-11 18:09:41 +0200

    Dodano możliwość przesuwania elementów listy za pomocą biblioteki vuedraggable

--- frontend/src/components/ViewList.vue
+++ frontend/src/components/ViewList.vue
@@ -2,9 +2,8 @@
     <v-row no-gutters>
         <v-col cols="3" class="pa-2">
         </v-col>
-        <v-col cols="6" v-if="listInfo && listEntries" class="pa-2">
+        <v-col cols="6" v-if="ready" class="pa-2">
             <v-card
-                variant=""
                 :title="listInfo.name"   
             >
                 <template v-slot:subtitle>
@@ -22,48 +21,63 @@
                 <v-card-text>{{ listInfo.description }}</v-card-text>
             </v-card>
             <v-divider :thickness="4"></v-divider>
-            <div v-for="entry in listEntries" class="pa-2">
-                <v-card
-                    variant="outlined"
-                >
-                <div class="d-flex flex-no-wrap justify-space-between">
-                    <div>
-                        <v-card-title>{{ entry.name }}</v-card-title>
-                        <v-card-subtitle>{{ entry.country }}-{{ entry.city }}-{{ entry.location }}</v-card-subtitle>
-                        <v-card-text>
-                            <v-rating
-                                v-if="entry.ratingAndId['0']!=0"
-                                v-model="entry.ratingAndId['0']"
-                                half-increments
-                                readonly
-                                size="small"
-                                density="compact"
-                                color="cyan"
-                            ></v-rating><br>
-                            <v-btn 
-                                v-if="entry.ratingAndId['1']!=0"
-                                class="bg-cyan text-white"
-                                text="Read review"
-                                size="small"
-                                @click="navigateTo({
-                                    name: 'reviewComments',
-                                    params: {
-                                        attractionId: entry.attraction_id,
-                                        reviewId: entry.ratingAndId['1']
-                                    }
-                                })"></v-btn>
-                        </v-card-text>
+            <draggable
+                :list="listEntries"
+                :disabled="!enabled"
+                item-key="name"
+                class="list-group"
+                ghost-class="ghost"
+                :move="checkMove"
+                @start="dragging = true"
+                @end="onEnd"
+            >
+                <template #item="{ element }">
+                    <div class="list-group-item" :class="{ 'not-draggable': !enabled }">
+                        <v-card
+                            variant="outlined"
+                        >
+                            <div class="d-flex flex-no-wrap justify-space-between">
+                                <div>
+                                    <v-card-title>{{ element.name }}</v-card-title>
+                                    <v-card-subtitle>
+                                        {{ element.country }}-{{ element.city }}-{{ element.location }}
+                                    </v-card-subtitle>
+                                    <v-card-text>
+                                        <v-rating
+                                            v-if="element.ratingAndId['0']!=0"
+                                            v-model="element.ratingAndId['0']"
+                                            half-increments
+                                            readonly
+                                            size="small"
+                                            density="compact"
+                                            color="cyan"
+                                        ></v-rating><br>
+                                        <v-btn 
+                                            v-if="element.ratingAndId['1']!=0"
+                                            class="bg-cyan text-white"
+                                            text="Read review"
+                                            size="small"
+                                            @click="navigateTo({
+                                                name: 'reviewComments',
+                                                params: {
+                                                    attractionId: element.attraction_id,
+                                                    reviewId: element.ratingAndId['1']
+                                                }
+                                            })"></v-btn>
+                                    </v-card-text>
+                                </div>
+                                <v-avatar 
+                                    class="ma-3"
+                                    rounded="0"
+                                    size="125"
+                                >
+                                    <v-img :src="element.imageUrl"></v-img>
+                                </v-avatar>
+                            </div>
+                        </v-card>
                     </div>
-                    <v-avatar 
-                        class="ma-3"
-                        rounded="0"
-                        size="125"
-                    >
-                        <v-img :src="entry.imageUrl"></v-img>
-                    </v-avatar>
-                </div>
-                </v-card>
-            </div>
+                </template>
+            </draggable>
         </v-col>
     </v-row>
 </template>
@@ -71,21 +85,30 @@
 <script>
 import ListService from '@/services/ListService';
 import UserService from '@/services/UserService';
+import draggable from 'vuedraggable'
 export default {
     data () {
         return {
             listInfo: null,
-            listEntries: null
+            listEntries: null,
+            ready: false,
+            enabled: true,
+            dragging: false
         }
+    },
+    components: {
+        draggable
     },
     async mounted() {
         this.listInfo = (await ListService.getList(this.$route.params.listId)).data
         this.listEntries = (await ListService.getListEntries(this.$route.params.listId)).data
+        console.log(this.listEntries)
         const reviews = (await UserService.getReviews(this.listInfo.user.user_id)).data
         this.listEntries = this.listEntries.map(entry => ({
             ...entry,
             ratingAndId: this.findRating(entry.attraction_id, reviews)
         }))
+        this.ready = true
     },
     methods: {
         navigateTo(route) {
@@ -95,6 +118,13 @@ export default {
         findRating(attractionId, reviews) {
             const review = reviews.find(element => element.attractionId === attractionId);
             return review ? [review.review.rating, review.review.review_id] : [0, 0];
+        },
+        checkMove(e) {
+            window.console.log("Future index: " + e.draggedContext.futureIndex);
+        },
+        onEnd() {
+            this.dragging = false
+            console.log(this.listEntries)
         }
     }
 }
@@ -104,5 +134,13 @@ export default {
 .link{
     cursor: pointer;
     display: inline-block;
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.not-draggable {
+  cursor: no-drop;
 }
 </style>
